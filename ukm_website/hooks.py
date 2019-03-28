67d23bebdaf07dd54f44d76cbac3c615b8d6907a
wellart <wella@integria.co.id>
2019-03-28 15:32:53 -0400

    edit hook

--- ukm_website/hooks.py
+++ ukm_website/hooks.py
@@ -35,7 +35,7 @@ app_license = "MIT"
 # ----------
 
 # application home page (will override Website Settings)
- home_page = "index"
+ #home_page = "index"
 
 # website user home page (by Role)
 # role_home_page = {
